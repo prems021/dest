f019da9b2df47aea43f87a25cdd7ece2ea59384e
prems021 <premji.netcom@gmail.com>
2016-12-01 11:56:55 +0530

    Create about.component.ts

---
+++ app/components/about/about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Http } from "@angular/http";
+import 'rxjs/add/operator/map';
+
+@Component({
+    selector: 'about',
+    templateUrl: 'components/about/about.component.html',
+    styleUrls: ['components/about/about.component.css']
+})
+export class AboutComponent {
+    
+}
