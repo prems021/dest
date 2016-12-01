4651de81308634d389787c4837435474a5764385
prems021 <premji.netcom@gmail.com>
2016-12-01 13:00:15 +0530

    Update systemjs.config.js

--- public/js/systemjs.config.js
+++ public/js/systemjs.config.js
@@ -1,50 +1,38 @@
-<html>
-  <head>
-    <script>document.write('<base href="' + document.location + '" />');</script>
-    <title> Destiny </title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/style.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-     <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-
-    <style>
-@font-face {
-			font-family: 'Aldo';
-			src: url('/font/Aldo-SemiBold.eot');
-			src: url('/font/Aldo-SemiBold.eot?#iefix') format('embedded-opentype'),
-				url('/font/Aldo-SemiBold.woff') format('woff'),
-				url('/font/Aldo-SemiBold.ttf') format('truetype');
-			font-weight: 600;
-			font-style: normal;
-		}
-</style>
-    <base href="/">
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
-
-    <!-- Polyfill(s) for older browsers -->
-         <script src="js/vendor/shim.min.js"></script>
-    <script src="js/vendor/zone.min.js"></script>
-    <script src="js/vendor/Reflect.js"></script>
-    <script src="js/vendor/system.js"></script>
-
-
-    <script src="js/systemjs.config.js"></script>
-    <script>
-      System.import('app').catch(function(err) { console.error(err); });
-    </script>
-
-  </head>
-
-  <!-- 3. Display the application -->
-  <body>
-    <my-app>Loading...</my-app>
-  </body>
-</html>
+(function (global) {
+  System.config({
+    paths: {
+      // paths serve as alias
+      'npm:': 'node_modules/'
+    },
+    // map tells the System loader where to look for things
+    map: {
+      // our app is within the app folder
+      app: 'app',
+      // angular bundles
+      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+      '@angular/http/testing': 'npm:@angular/http/bundles/http-testing.umd.js',
+      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      '@angular/material': 'npm:@angular/material/material.umd.js',
+      '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
+      // other libraries
+      'rxjs':                      'npm:rxjs',
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+    },
+    // packages tells the System loader how to load when no filename and/or no extension
+    packages: {
+      app: {
+        main: './main.js',
+        defaultExtension: 'js'
+      },
+      rxjs: {
+        defaultExtension: 'js'
+      }
+    }
+  });
+})(this);
